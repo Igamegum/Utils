83d97c96a34d51456c53edf7c5ac242ff163c4eb
igamegum <email@igamegum.com>
2017-05-11 10:33:55 +0800

    2017-5-11-10-33

--- thinkjoy_utils.h
+++ thinkjoy_utils.h
@@ -12,6 +12,32 @@
 //#include <jsoncpp/json.h>
 #include <glog/logging.h>
 
+void Encrypt(const std::string input_filename,const std::string output_filename,const std::string Key)
+{
+	
+	std::ifstream ifs;
+	ifs.open(input_filename,std::ios::in|std::ios::binary);
+
+
+	std::ofstream ofs;
+	ofs.open(output_filename,std::ios::out|std::ios::binary);
+
+	char ch;
+
+	long long  index = 0;
+	int length = Key.length();
+
+	while(!ifs.eof())
+	{
+		ifs.read(&ch,1);
+		ofs<< static_cast<char>((ch^( char )Key[index % length]));
+		++index;
+	}
+
+	ifs.close();
+	ofs.close();
+}
+
 
 
 int get_return_code(zframe_t *self)
